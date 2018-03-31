93681c931e6823076e0683574faf076c00347478
Michiel de Graaf <graafmichiel@gmail.com>
2018-03-31 22:29:48 +0200

    Forgot what this is...

--- build/assets/javascripts/application.js
+++ build/assets/javascripts/application.js
@@ -5,4 +5,25 @@
     });
   }).call(this);
 
+  $(document).ready(function() {
+    $('.list-main li').mouseenter(function(e) {
+      var x;
+      x = e.pageX - ($(this).offset().left);
+      $(this).css('z-index', '20').children('.tooltip').css({
+        'margin-left': x + 20,
+        'display': 'block'
+      });
+    }).mousemove(function(e) {
+      var x;
+      x = e.pageX - ($(this).offset().left);
+      $(this).children('div.tooltip').css({
+        'margin-left': x + 20
+      });
+    }).mouseleave(function() {
+      $(this).css('z-index', '1').children('.tooltip').animate({
+        'opacity': 'hide'
+      }, 'fast');
+    });
+  });
+
 }).call(this);
