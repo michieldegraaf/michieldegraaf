87b039a60996924c6193b5d28bec6bcd47334733
Michiel de Graaf <graafmichiel@gmail.com>
2018-04-05 21:59:40 +0200

    Build

--- build/assets/javascripts/application.js
+++ build/assets/javascripts/application.js
@@ -1,29 +1,8 @@
 (function() {
   (function() {
-    return $(".logo").click(function() {
+    return $(".logo span").click(function() {
       $("body").toggleClass("contrast");
     });
   }).call(this);
-
-  $(document).ready(function() {
-    $('.list-main li').mouseenter(function(e) {
-      var x;
-      x = e.pageX - ($(this).offset().left);
-      $(this).css('z-index', '20').children('.tooltip').css({
-        'margin-left': x + 20,
-        'display': 'block'
-      });
-    }).mousemove(function(e) {
-      var x;
-      x = e.pageX - ($(this).offset().left);
-      $(this).children('div.tooltip').css({
-        'margin-left': x + 20
-      });
-    }).mouseleave(function() {
-      $(this).css('z-index', '1').children('.tooltip').animate({
-        'opacity': 'hide'
-      }, 'fast');
-    });
-  });
 
 }).call(this);
